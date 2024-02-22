1fcfc26405900e40e1b62abc1c639dba57f94178
Guilherme <guisou1902@gmail.com>
2024-02-22 15:36:20 -0300

    feat: added responsiviness

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -24,41 +24,41 @@ export default function page() {
 					text={`Painel Administrativo (${whichRole()})`}
 				></H1>
 				<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 lg:gap-16">
-					<div className="flex flex-col items-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
-						<p className="text-[#BEBEBE] text-2xl">
+					<div className="flex flex-col items-center text-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
+						<p className="text-[#BEBEBE] md:text-2xl">
 							Número de Membros
 						</p>
-						<h2 className="text-3xl">89</h2>
+						<h2 className="text-2xl md:text-3xl">89</h2>
 					</div>
-					<div className="flex flex-col items-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
-						<p className="text-[#BEBEBE] text-2xl">
+					<div className="flex flex-col items-center text-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
+						<p className="text-[#BEBEBE] md:text-2xl">
 							Número de Batismos
 						</p>
-						<h2 className="text-3xl">4</h2>
+						<h2 className="text-2xl md:text-3xl">4</h2>
 					</div>
-					<div className="flex flex-col items-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
-						<p className="text-[#BEBEBE] text-2xl">
+					<div className="flex flex-col items-center text-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
+						<p className="text-[#BEBEBE] md:text-2xl">
 							Próximo Evento
 						</p>
-						<h2 className="text-3xl">24/02</h2>
+						<h2 className="text-2xl md:text-3xl">24/02</h2>
 					</div>
 				</div>
 				<div>
-					<div className="flex flex-col items-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
-						<p className="text-[#BEBEBE] text-2xl">
+					<div className="flex flex-col items-center text-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
+						<p className="text-[#BEBEBE] md:text-2xl">
 							Última atividade
 						</p>
-						<h2 className="text-3xl">
+						<h2 className="text-2xl md:text-3xl">
 							2 Novos membros adicionados
 						</h2>
 					</div>
 				</div>
 				<div>
-					<div className="flex flex-col items-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
-						<p className="text-[#BEBEBE] text-2xl">
+					<div className="flex flex-col items-center text-center p-4 text-white bg-[#0055D4] rounded-2xl font-bold w-full">
+						<p className="text-[#BEBEBE] md:text-2xl">
 							Última sessão administrativa
 						</p>
-						<h2 className="text-3xl">04/02/2024</h2>
+						<h2 className="text-2xl md:text-3xl">04/02/2024</h2>
 					</div>
 				</div>
 			</div>
