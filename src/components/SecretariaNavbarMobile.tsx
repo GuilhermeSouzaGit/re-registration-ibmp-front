ff06cf8e08f5f89cb1daf4bb0c5d78b0310eb736
Guilherme <guisou1902@gmail.com>
2024-02-22 15:36:39 -0300

    feat: added responsiviness

--- src/components/SecretariaNavbarMobile.tsx
+++ src/components/SecretariaNavbarMobile.tsx
@@ -25,60 +25,64 @@ export const SecretariaNavbarMobile = () => {
 					width={20}
 					height={20}
 					alt="ícone do menu"
-					className="hover:cursor-pointer"
+					className="hover:cursor-pointer inline lg:hidden"
 					onClick={() => {
 						setMenu(true);
 					}}
 				></Image>
 			)}
 			<ul
-				className={`flex flex-col fixed z-10 bg-[#0055D4] top-0 ${menu ? "left-0" : "-left-80"} transition-all lg:hidden gap-8 text-white rounded px-8 h-full xl:text-xl font-inter font-semibold`}
+				className={`flex flex-col fixed py-16 z-10 bg-[#0055D4] top-0 ${menu ? "left-0" : "-left-80"} transition-all lg:hidden gap-8 text-white rounded px-4 md:px-8 h-full text-xs md:text-base xl:text-xl font-inter font-semibold`}
 			>
 				<Link
 					href="/"
-					className="flex gap-2 hover:scale-110 transition-all"
+					className="flex gap-2 items-center hover:scale-110 transition-all"
 				>
 					<Image
 						src="/users-icon.svg"
-						width={30}
-						height={30}
+						width={20}
+						height={20}
 						alt="Ícone de usuários"
+						className="md:w-8 md:h-8"
 					/>
 					Gestão de membros
 				</Link>
 				<Link
 					href="/"
-					className="flex gap-2 hover:scale-110 transition-all"
+					className="flex gap-2 items-center hover:scale-110 transition-all"
 				>
 					<Image
 						src="/relatorio-icon.svg"
-						width={20}
-						height={20}
+						width={15}
+						height={15}
 						alt="Ícone de relatório"
+						className="md:w-8 md:h-8"
 					/>
 					Atas e relatórios
 				</Link>
 				<Link
 					href="/"
-					className="flex gap-2 hover:scale-110 transition-all"
+					className="flex gap-2 items-center hover:scale-110 transition-all"
 				>
 					<Image
 						src="/baptism-icon.svg"
-						width={30}
-						height={30}
+						width={20}
+						height={20}
 						alt="Ícone de batismo"
+						className="md:w-8 md:h-8"
 					/>
 					Gestão de batismos
 				</Link>
 				<Link
 					href="/"
-					className="flex gap-2 hover:scale-110 transition-all"
+					className="flex gap-2 items-center hover:scale-110 transition-all"
 				>
 					<Image
 						src="/calendar-icon.svg"
-						width={25}
-						height={25}
+						width={18}
+						height={18}
 						alt="Ícone de calendário"
+						className="md:w-8 md:h-8"
 					/>
 					Calendário e eventos
 				</Link>
@@ -89,9 +93,10 @@ export const SecretariaNavbarMobile = () => {
 				>
 					<Image
 						src="/logout-icon.svg"
-						width={25}
-						height={25}
+						width={18}
+						height={18}
 						alt="Ícone de logout"
+						className="md:w-8 md:h-8"
 					/>
 				</Link>
 			</ul>
