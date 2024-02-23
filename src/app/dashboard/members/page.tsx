d030486d64d0817ea871b604d2070443a31642af
Guilherme <guisou1902@gmail.com>
2024-02-23 16:51:24 -0300

    feat: finished the Members table

--- src/app/dashboard/members/page.tsx
+++ src/app/dashboard/members/page.tsx
@@ -10,6 +10,11 @@ interface Member {
 	name: string;
 	memberPhoto: string;
 	email: string;
+	dateOfBirth: string;
+	gender: string;
+	phone: string;
+	maritalStatus: string;
+	baptismDate: string;
 }
 
 export default function page() {
@@ -32,17 +37,17 @@ export default function page() {
 				console.log(e);
 			});
 	}, [token]);
-
+	// overflow-x-scroll scroll whitespace-nowrap scroll-smooth
 	return (
 		<>
 			<MainNav />
 			<div className="flex flex-col gap-8 p-4 md:p-8 xl:p-16">
 				<Button action="/dashboard" text="Voltar" />
 				<H1 text="Gestão de membros (89)" />
-				<div className="flex max-w-full rounded-[32px] overflow-hidden">
-					<div className="grid grid-cols-6 w-[4000px]">
-						<div className="flex flex-col w-fit">
-							<h1 className="text-center bg-[#0055D4] text-white font-medium py-2">
+				<div className="flex max-w-fit rounded-xl overflow-hidden">
+					<div className="flex overflow-x-scroll scroll whitespace-nowrap scroll-smooth">
+						<div className="flex flex-col w-max">
+							<h1 className="text-center text-lg bg-[#0055D4] text-white font-medium py-2">
 								Nome
 							</h1>
 							{members.length > 0 ? (
@@ -71,7 +76,111 @@ export default function page() {
 									</div>
 								))
 							) : (
-								<p>Nenhum membro encontrado.</p>
+								<p>Carregando</p>
+							)}
+						</div>
+						<div className="flex flex-col w-fit">
+							<h1 className="text-center text-lg bg-[#0055D4] text-white font-medium py-2">
+								Nascimento
+							</h1>
+							{members.length > 0 ? (
+								members.map((member, index) => (
+									<div
+										key={index}
+										className={`${index % 2 == 0 ? "bg-[#002D70]" : "bg-[#0055D4]"} flex justify-center items-center gap-4 w-full px-8 py-4`}
+									>
+										<p className="text-white font-semibold flex flex-col py-5">
+											{member.dateOfBirth}
+										</p>
+									</div>
+								))
+							) : (
+								<p>Carregando</p>
+							)}
+						</div>
+						<div className="flex flex-col w-fit">
+							<h1 className="text-center text-lg bg-[#0055D4] text-white font-medium py-2">
+								Sexo
+							</h1>
+							{members.length > 0 ? (
+								members.map((member, index) => (
+									<div
+										key={index}
+										className={`${index % 2 == 0 ? "bg-[#002D70]" : "bg-[#0055D4]"} flex justify-center items-center gap-4 px-8 py-4`}
+									>
+										<p className="text-white font-semibold flex flex-col py-5">
+											{member.gender === undefined
+												? "Não informado"
+												: member.gender}
+										</p>
+									</div>
+								))
+							) : (
+								<p>Carregando</p>
+							)}
+						</div>
+						<div className="flex flex-col w-fit">
+							<h1 className="text-center text-lg bg-[#0055D4] text-white font-medium py-2">
+								Telefone
+							</h1>
+							{members.length > 0 ? (
+								members.map((member, index) => (
+									<div
+										key={index}
+										className={`${index % 2 == 0 ? "bg-[#002D70]" : "bg-[#0055D4]"} flex justify-center items-center gap-4 px-8 py-4`}
+									>
+										<p className="text-white font-semibold flex flex-col py-5">
+											{member.phone === undefined
+												? "Não informado"
+												: member.phone}
+										</p>
+									</div>
+								))
+							) : (
+								<p>Carregando</p>
+							)}
+						</div>
+						<div className="flex flex-col w-fit">
+							<h1 className="text-center text-lg bg-[#0055D4] text-white font-medium py-2">
+								Estado Civil
+							</h1>
+							{members.length > 0 ? (
+								members.map((member, index) => (
+									<div
+										key={index}
+										className={`${index % 2 == 0 ? "bg-[#002D70]" : "bg-[#0055D4]"} flex justify-center items-center gap-4 px-8 py-4`}
+									>
+										<p className="text-white font-semibold flex flex-col py-5">
+											{member.maritalStatus === undefined
+												? "Não informado"
+												: member.maritalStatus}
+										</p>
+									</div>
+								))
+							) : (
+								<p>Carregando</p>
+							)}
+						</div>
+						<div className="flex flex-col w-fit">
+							<h1 className="text-center text-lg bg-[#0055D4] text-white font-medium py-2">
+								Batismo
+							</h1>
+							{members.length > 0 ? (
+								members.map((member, index) => (
+									<div
+										key={index}
+										className={`${index % 2 == 0 ? "bg-[#002D70]" : "bg-[#0055D4]"} flex justify-center items-center gap-4 px-8 py-4`}
+									>
+										<p className="text-white font-semibold flex flex-col py-5">
+											{member.baptismDate === undefined ||
+											member.baptismDate === ""
+												? "Não informado"
+												: member.baptismDate}
+										</p>
+									</div>
+								))
+							) : (
+								<p>Carregando</p>
 							)}
 						</div>
 					</div>
